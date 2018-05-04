e43668b6619b337a3856c87a407078d90b9f31f3
nimmividya <compvidya@gmail.com>
2018-05-04 17:46:47 +0900

    files 6 to 11

---
+++ 7.Remotes/my-git-repo/style.css
@@ -0,0 +1,14 @@
+body {
+  padding: 20px;
+  font-family: Verdana, Arial, Helvetica, sans-serif;
+  font-size: 14px;
+  color: #111;
+}
+
+p, ul {
+  margin-bottom: 10px;
+}
+
+ul {
+  margin-left: 20px;
+}
